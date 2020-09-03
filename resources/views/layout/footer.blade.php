fa95b801e6fd49b33d718cae421147f3e79a96cd
adwindea <danadwnd@gmail.com>
2020-09-03 08:33:09 +0700

    edit footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
-<strong>Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+<strong>Copyright © <a href="{{url('/')}}">DawnIdea</a>.</strong>
 All rights reserved.
 <div class="float-right d-none d-sm-inline-block">
-  <b>Version</b> 3.0.2
+  <b>Version</b> 1.0.2
 </div>
