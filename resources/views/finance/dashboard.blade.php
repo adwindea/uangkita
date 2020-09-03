96e681fe0509847c1cd262fef0e1e66c021abc07
adwindea <danadwnd@gmail.com>
2020-09-03 15:52:23 +0700

    fix highcharts issue

--- resources/views/finance/dashboard.blade.php
+++ resources/views/finance/dashboard.blade.php
@@ -101,21 +101,6 @@
 </div>
 
 <script type="text/javascript">
-    Highcharts.setOptions({
-        colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
-            return {
-                radialGradient: {
-                    cx: 0.5,
-                    cy: 0.3,
-                    r: 0.7
-                },
-                stops: [
-                    [0, color],
-                    [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
-                ]
-            };
-        })
-    });
     Highcharts.chart('summary-chart', {
 		chart: {
 			type: 'column'
--- resources/views/finance/index.blade.php
+++ resources/views/finance/index.blade.php
@@ -54,5 +54,20 @@
             url = url.replace(':param', month);
             $('#dashboard-content').load(url);
         });
+        Highcharts.setOptions({
+            colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+                return {
+                    radialGradient: {
+                        cx: 0.5,
+                        cy: 0.3,
+                        r: 0.7
+                    },
+                    stops: [
+                        [0, color],
+                        [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+                    ]
+                };
+            })
+        });
     </script>
 @endsection
