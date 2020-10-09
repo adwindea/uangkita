6e87326a7df257beb12aaf97810de6492c276857
adwindea <danadwnd@gmail.com>
2020-10-09 10:37:08 +0700

    fix bug

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -13,6 +13,6 @@ class Budget extends Model
     protected $guarded = [];
 
     public function category(){
-        return $this->belongsTo('App\Models\Category');
+        return $this->belongsTo('App\Models\Category', 'category');
     }
 }
